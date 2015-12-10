7c64b44d0f6143eca884999a1945d744f976ad98
Pradeep Manje Gowda <Pradeep88.sai@gmail.com>
2015-12-10 14:00:58 +0100

    Code Committed

---
+++ FirstProject/src/day1/examples/MyFirstClass.java
@@ -0,0 +1,17 @@
+package day1.examples;
+
+public class MyFirstClass {
+
+	/**This is my
+	 * First PRogram
+	 */
+	public static void main(String[] args) {
+		// First Program
+		System.out.println("Hello World");
+		int x = 10;
+		int y = 20;
+		int Result = x + y;
+		System.out.println("Result : " + Result);
+	}
+
+}
