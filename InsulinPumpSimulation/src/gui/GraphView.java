cb282ff5e85f061f999a692bee903b5789d738de
mgchethankumar@gmail.com <mgchethankumar@gmail.com>
2015-12-21 19:24:03 +0100

    with graph

--- InsulinPumpSimulation/src/gui/GraphView.java
+++ InsulinPumpSimulation/src/gui/GraphView.java
@@ -1,7 +1,11 @@
  package gui;
 
+import java.awt.Color;
+import java.text.SimpleDateFormat;
+
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.wb.swt.SWTResourceManager;
@@ -11,6 +15,19 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.DateTime;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.time.Month;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.experimental.chart.swt.ChartComposite;
+import org.jfree.experimental.chart.swt.demo.SWTBarChartDemo1;
+import org.jfree.ui.RectangleInsets;
 
 public class GraphView {
 
@@ -24,6 +41,7 @@ public class GraphView {
 		try {
 			GraphView window = new GraphView();
 			window.open();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -34,8 +52,24 @@ public class GraphView {
 	 */
 	public void open() {
 		Display display = Display.getDefault();
-		createContents();
+		createContents();;
 		shlGraphView.open();
+		shlGraphView.pack();
+		
+		Label lblTo_1 = new Label(shlGraphView, SWT.NONE);
+		lblTo_1.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		lblTo_1.setBounds(224, 29, 26, 36);
+		lblTo_1.setText("To");
+		
+		Button btnApply = new Button(shlGraphView, SWT.NONE);
+		btnApply.setBounds(416, 29, 106, 36);
+		btnApply.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			shlGraphView.close();
+			}
+		});
+		btnApply.setText("Apply");
 		shlGraphView.layout();
 		while (!shlGraphView.isDisposed()) {
 			if (!display.readAndDispatch()) {
@@ -43,23 +77,102 @@ public class GraphView {
 			}
 		}
 	}
+    private static JFreeChart createChart(XYDataset dataset) {
+
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(
+            "insulin sugar level graph",  // title
+            "Date",             // x-axis label
+            "sugar level",   // y-axis label
+            dataset,            // data
+            true,               // create legend?
+            true,               // generate tooltips?
+            false               // generate URLs?
+        );
+
+        chart.setBackgroundPaint(Color.white);
+
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
+        plot.setDomainCrosshairVisible(true);
+        plot.setRangeCrosshairVisible(true);
+        
+        XYItemRenderer r = plot.getRenderer();
+        if (r instanceof XYLineAndShapeRenderer) {
+            XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) r;
+            renderer.setBaseShapesVisible(true);
+            renderer.setBaseShapesFilled(true);
+        }
+        
+        DateAxis axis = (DateAxis) plot.getDomainAxis();
+        axis.setDateFormatOverride(new SimpleDateFormat("MMM-yyyy"));
+        
+        return chart;
+
+    }
+    private static XYDataset createDataset() {
+
+        TimeSeries s1 = new TimeSeries("insulin sugar level");
+        s1.add(new Month(2, 2001), 181.8);
+        s1.add(new Month(3, 2001), 167.3);
+        s1.add(new Month(4, 2001), 153.8);
+        s1.add(new Month(5, 2001), 167.6);
+        s1.add(new Month(6, 2001), 158.8);
+        s1.add(new Month(7, 2001), 148.3);
+        s1.add(new Month(8, 2001), 153.9);
+        s1.add(new Month(9, 2001), 142.7);
+        s1.add(new Month(10, 2001), 123.2);
+        s1.add(new Month(11, 2001), 131.8);
+        s1.add(new Month(12, 2001), 139.6);
+        s1.add(new Month(1, 2002), 142.9);
+        s1.add(new Month(2, 2002), 138.7);
+        s1.add(new Month(3, 2002), 137.3);
+        s1.add(new Month(4, 2002), 143.9);
+        s1.add(new Month(5, 2002), 139.8);
+        s1.add(new Month(6, 2002), 137.0);
+        s1.add(new Month(7, 2002), 132.8);
+
+
+        
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+        dataset.addSeries(s1);
+       
+        
+        return dataset;
+    }
 
 	/**
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		shlGraphView = new Shell(Display.getDefault(), SWT.TITLE|SWT.CLOSE|SWT.BORDER);
+		
+		shlGraphView = new Shell(Display.getCurrent(), SWT.TITLE|SWT.CLOSE|SWT.BORDER);
 		shlGraphView.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-		shlGraphView.setSize(590, 408);
+		shlGraphView.setSize(1000, 517);
 		shlGraphView.setText("Graph View");
 		
 		Group groupGraph = new Group(shlGraphView, SWT.NONE);
+		
 		groupGraph.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-		groupGraph.setBounds(10, 50, 554, 309);
+	       
+		groupGraph.setBounds(10, 80, 674, 394);
+		groupGraph.setLayout(new FillLayout());
+		groupGraph.setText("Time series demo for jfreechart running with SWT");
+		
+		JFreeChart chart = createChart(createDataset());
+		ChartComposite frame = new ChartComposite(groupGraph, SWT.NONE, chart, true);
+		frame.setChart(chart);
+		 frame.setDisplayToolTips(true);
+	        frame.setHorizontalAxisTrace(true);
+	        frame.setVerticalAxisTrace(true);
+	    	
+	        
 		
 		Label lblFrom = new Label(shlGraphView, SWT.NONE);
 		lblFrom.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-		lblFrom.setBounds(10, 29, 34, 15);
+		lblFrom.setBounds(10, 29, 59, 36);
 		lblFrom.setText("From");
 		
 		Label lblTo = new Label(shlGraphView, SWT.NONE);
@@ -67,22 +180,11 @@ public class GraphView {
 		lblTo.setBounds(138, 29, 14, 15);
 		lblTo.setText("To");
 		
-		Button btnApply = new Button(shlGraphView, SWT.NONE);
-		btnApply.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-			shlGraphView.close();
-			}
-		});
-		btnApply.setBounds(263, 19, 75, 25);
-		btnApply.setText("Apply");
-		
 		DateTime dateTimeFrom = new DateTime(shlGraphView, SWT.BORDER);
-		dateTimeFrom.setBounds(50, 20, 80, 24);
+		dateTimeFrom.setBounds(62, 29, 134, 36);
 		
 		DateTime dateTimeTo = new DateTime(shlGraphView, SWT.BORDER);
-		dateTimeTo.setBounds(159, 19, 80, 24);
+		dateTimeTo.setBounds(256, 29, 134, 36);
 
 	}
-
 }
--- InsulinPumpSimulation/src/gui/HomeScreen.java
+++ InsulinPumpSimulation/src/gui/HomeScreen.java
@@ -6,176 +6,234 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.ProgressBar;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.wb.swt.SWTResourceManager;
+
+import java.awt.Color;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import javax.swing.Timer;
+
 import org.eclipse.swt.browser.Browser;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.time.Millisecond;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.experimental.chart.swt.ChartComposite;
 
 /**
  * Newly Imported from google.
  */
 
-public class HomeScreen {
+public class HomeScreen{
+
+	/** Timer to refresh graph after every 1/4th of a second */
+	//private Time timer = new Timer(1000, this);
 
 	protected Shell shlHomeScreen;
-	protected Label lblClock ;
+	protected Label lblClock;
+	final JFreeChart chart;
 	
+	private TimeSeries series;
+
+    /** The most recent value added. */
+    private double lastValue = 100.0;
+
 	/**
 	 * Launch the application.
+	 * 
 	 * @param args
 	 */
-	
-		
+
+	public HomeScreen() {
+        this.series = new TimeSeries("Random Data", Millisecond.class);
+
+        final TimeSeriesCollection dataset = new TimeSeriesCollection(this.series);
+        chart = createChart(dataset);
+
+        //Sets background color of chart
+        chart.setBackgroundPaint(Color.LIGHT_GRAY);
+	}
+
 	public static void main(String[] args) {
 
 		try {
 			HomeScreen window = new HomeScreen();
 			window.open();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	
 	public void open() {
+		//timer.setInitialDelay(1000);
 		Display display = Display.getDefault();
 		createContents();
 		shlHomeScreen.open();
 		shlHomeScreen.layout();
+		//timer.start();
 		while (!shlHomeScreen.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
 		}
 	}
-	
 
 	/**
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		shlHomeScreen = new Shell(Display.getDefault(), SWT.TITLE|SWT.CLOSE|SWT.BORDER);
-		shlHomeScreen.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		shlHomeScreen = new Shell(Display.getDefault(), SWT.TITLE | SWT.CLOSE
+				| SWT.BORDER);
+		shlHomeScreen.setBackground(SWTResourceManager
+				.getColor(SWT.COLOR_WHITE));
 		shlHomeScreen.setSize(748, 520);
 		shlHomeScreen.setText("TWO HARMONE SIMULATOR PUMP : GROUP D");
-		
+
 		Group group = new Group(shlHomeScreen, SWT.NONE);
 		group.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		group.setBounds(10, 67, 441, 61);
-		
+
 		Label lblLatestSugarLevels = new Label(group, SWT.NONE);
 		lblLatestSugarLevels.setBounds(10, 23, 151, 23);
-		lblLatestSugarLevels.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-		lblLatestSugarLevels.setFont(SWTResourceManager.getFont("Calibri", 14, SWT.BOLD));
+		lblLatestSugarLevels.setBackground(SWTResourceManager
+				.getColor(SWT.COLOR_WHITE));
+		lblLatestSugarLevels.setFont(SWTResourceManager.getFont("Calibri", 14,
+				SWT.BOLD));
 		lblLatestSugarLevels.setText("Latest Sugar Levels");
-		
+
 		Label lblMgdl = new Label(group, SWT.NONE);
 		lblMgdl.setBounds(176, 29, 51, 15);
 		lblMgdl.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		lblMgdl.setText("??? mg/dl");
-		
+
 		Label lblNewLabel = new Label(group, SWT.NONE);
 		lblNewLabel.setBounds(267, 29, 159, 15);
 		lblNewLabel.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		lblNewLabel.setText("Expected Value 70 - 120 mg/dl");
-		
+
 		Group grpCriticalIndicators = new Group(shlHomeScreen, SWT.NONE);
-		grpCriticalIndicators.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-		grpCriticalIndicators.setFont(SWTResourceManager.getFont("Calibri", 12, SWT.BOLD));
+		grpCriticalIndicators.setBackground(SWTResourceManager
+				.getColor(SWT.COLOR_WHITE));
+		grpCriticalIndicators.setFont(SWTResourceManager.getFont("Calibri", 12,
+				SWT.BOLD));
 		grpCriticalIndicators.setText("Critical Indicators");
 		grpCriticalIndicators.setBounds(457, 50, 265, 381);
-		
-		ProgressBar progressBarBattery = new ProgressBar(grpCriticalIndicators, SWT.NONE);
-		progressBarBattery.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+
+		ProgressBar progressBarBattery = new ProgressBar(grpCriticalIndicators,
+				SWT.NONE);
+		progressBarBattery.setBackground(SWTResourceManager
+				.getColor(SWT.COLOR_WHITE));
 		progressBarBattery.setBounds(132, 32, 123, 25);
-		
+
 		Label lblBattery = new Label(grpCriticalIndicators, SWT.NONE);
 		lblBattery.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		lblBattery.setBounds(10, 42, 55, 15);
 		lblBattery.setText("Battery");
-		
-		ProgressBar progressBarInsulin = new ProgressBar(grpCriticalIndicators, SWT.NONE);
-		progressBarInsulin.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+
+		ProgressBar progressBarInsulin = new ProgressBar(grpCriticalIndicators,
+				SWT.NONE);
+		progressBarInsulin.setBackground(SWTResourceManager
+				.getColor(SWT.COLOR_WHITE));
 		progressBarInsulin.setBounds(132, 79, 123, 25);
-		
-		ProgressBar progressBarGlucagon = new ProgressBar(grpCriticalIndicators, SWT.NONE);
-		progressBarGlucagon.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+
+		ProgressBar progressBarGlucagon = new ProgressBar(
+				grpCriticalIndicators, SWT.NONE);
+		progressBarGlucagon.setBackground(SWTResourceManager
+				.getColor(SWT.COLOR_WHITE));
 		progressBarGlucagon.setBounds(132, 131, 123, 25);
-		
+
 		Label lblInsulinReservoir = new Label(grpCriticalIndicators, SWT.NONE);
-		lblInsulinReservoir.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		lblInsulinReservoir.setBackground(SWTResourceManager
+				.getColor(SWT.COLOR_WHITE));
 		lblInsulinReservoir.setBounds(10, 89, 102, 15);
 		lblInsulinReservoir.setText("Insulin Reservoir");
-		
+
 		Label lblGlucagonReservoir = new Label(grpCriticalIndicators, SWT.NONE);
-		lblGlucagonReservoir.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		lblGlucagonReservoir.setBackground(SWTResourceManager
+				.getColor(SWT.COLOR_WHITE));
 		lblGlucagonReservoir.setBounds(10, 141, 116, 15);
 		lblGlucagonReservoir.setText("Glucagon Reservoir");
-		
+
 		Label lblNewLabel_1 = new Label(grpCriticalIndicators, SWT.NONE);
-		lblNewLabel_1.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		lblNewLabel_1.setBackground(SWTResourceManager
+				.getColor(SWT.COLOR_WHITE));
 		lblNewLabel_1.setBounds(10, 207, 116, 15);
 		lblNewLabel_1.setText("Pump");
-		
+
 		Label lblNewLabel_2 = new Label(grpCriticalIndicators, SWT.NONE);
-		lblNewLabel_2.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		lblNewLabel_2.setBackground(SWTResourceManager
+				.getColor(SWT.COLOR_WHITE));
 		lblNewLabel_2.setBounds(10, 251, 129, 15);
 		lblNewLabel_2.setText("Blood Glucose Sensor");
-		
+
 		Label lblNewLabel_3 = new Label(grpCriticalIndicators, SWT.NONE);
-		lblNewLabel_3.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		lblNewLabel_3.setBackground(SWTResourceManager
+				.getColor(SWT.COLOR_WHITE));
 		lblNewLabel_3.setBounds(10, 294, 116, 15);
 		lblNewLabel_3.setText("Needle Assembly");
-		
+
 		Label lblNewLabel_4 = new Label(grpCriticalIndicators, SWT.NONE);
-		lblNewLabel_4.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		lblNewLabel_4.setBackground(SWTResourceManager
+				.getColor(SWT.COLOR_WHITE));
 		lblNewLabel_4.setBounds(10, 342, 116, 15);
 		lblNewLabel_4.setText("Alarm");
-		
+
 		Label label = new Label(grpCriticalIndicators, SWT.NONE);
 		label.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		label.setBounds(171, 207, 55, 25);
 		label.setText("New Label");
-		
+
 		Label label_1 = new Label(grpCriticalIndicators, SWT.NONE);
 		label_1.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		label_1.setText("New Label");
-		label_1.setBounds(171, 251, 55, 25); 
-		
+		label_1.setBounds(171, 251, 55, 25);
+
 		Label label_2 = new Label(grpCriticalIndicators, SWT.NONE);
 		label_2.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		label_2.setText("New Label");
 		label_2.setBounds(171, 294, 55, 25);
-		
+
 		Label label_3 = new Label(grpCriticalIndicators, SWT.NONE);
 		label_3.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		label_3.setText("New Label");
 		label_3.setBounds(171, 342, 55, 25);
-		
+
 		Group grpNextBolusDosage = new Group(shlHomeScreen, SWT.NONE);
-		grpNextBolusDosage.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-		grpNextBolusDosage.setFont(SWTResourceManager.getFont("Calibri", 12, SWT.BOLD));
+		grpNextBolusDosage.setBackground(SWTResourceManager
+				.getColor(SWT.COLOR_WHITE));
+		grpNextBolusDosage.setFont(SWTResourceManager.getFont("Calibri", 12,
+				SWT.BOLD));
 		grpNextBolusDosage.setText("Next Bolus Dosage");
 		grpNextBolusDosage.setBounds(10, 133, 441, 61);
-		
+
 		Label lblDinner = new Label(grpNextBolusDosage, SWT.NONE);
 		lblDinner.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		lblDinner.setBounds(10, 25, 140, 20);
 		lblDinner.setText("Dinner ???");
-		
+
 		Button btnAdjustCarb = new Button(grpNextBolusDosage, SWT.NONE);
 		btnAdjustCarb.addSelectionListener(new SelectionAdapter() {
-			//.setVisible(true)@Override
+			// .setVisible(true)@Override
+			
 			public void widgetSelected(SelectionEvent e) {
 				Display display = Display.getDefault();
 				createContents();
 				CARBRemainderPage crp = new CARBRemainderPage();
 				crp.open(false);
-				while (null != crp.shlCarbAndRemainder && !crp.shlCarbAndRemainder.isDisposed()) {
+				while (null != crp.shlCarbAndRemainder
+						&& !crp.shlCarbAndRemainder.isDisposed()) {
 					if (!display.readAndDispatch()) {
 						display.sleep();
 					}
@@ -184,23 +242,51 @@ public class HomeScreen {
 		});
 		btnAdjustCarb.setBounds(348, 20, 75, 25);
 		btnAdjustCarb.setText("Adjust CARB");
-		
+
 		Label lblTimeLeftFor = new Label(grpNextBolusDosage, SWT.NONE);
-		lblTimeLeftFor.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		lblTimeLeftFor.setBackground(SWTResourceManager
+				.getColor(SWT.COLOR_WHITE));
 		lblTimeLeftFor.setBounds(171, 25, 171, 20);
 		lblTimeLeftFor.setText("Next Dinner Time");
+
+		Group graphDynamic = new Group(shlHomeScreen, SWT.NONE);
+		graphDynamic
+				.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		graphDynamic.setBounds(10, 200, 441, 191);
 		
-		Group group_2 = new Group(shlHomeScreen, SWT.NONE);
-		group_2.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-		group_2.setBounds(10, 200, 441, 191);
-		
+		graphDynamic.addListener(SWT.Activate, new Listener() {
+			
+			@Override
+			public void handleEvent(Event arg0) {
+				final double factor = 0.9 + 0.2 * Math.random();
+				lastValue = lastValue * factor;
+
+				final Millisecond now = new Millisecond();
+				series.add(new Millisecond(), lastValue);
+ 
+				System.out.println("Current Time in Milliseconds = " + now.toString()
+						+ ", Current Value : " + lastValue);
+				
+			}
+		});
+
+		// Dynamic Chart
+		ChartComposite frame = new ChartComposite(graphDynamic, SWT.NONE,
+				chart, true);
+		frame.setDisplayToolTips(true);
+		frame.setHorizontalAxisTrace(false);
+		frame.setVerticalAxisTrace(false);
+
 		Group grpActivityLog = new Group(shlHomeScreen, SWT.NONE);
-		grpActivityLog.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-		grpActivityLog.setFont(SWTResourceManager.getFont("Calibri", 12, SWT.BOLD));
+		grpActivityLog.setBackground(SWTResourceManager
+				.getColor(SWT.COLOR_WHITE));
+		grpActivityLog.setFont(SWTResourceManager.getFont("Calibri", 12,
+				SWT.BOLD));
 		grpActivityLog.setText("Activity Log");
 		grpActivityLog.setBounds(10, 389, 441, 82);
-		
-		StyledText TxtActivityLog = new StyledText(grpActivityLog, SWT.MULTI | SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
+
+		StyledText TxtActivityLog = new StyledText(grpActivityLog, SWT.MULTI
+				| SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
 		TxtActivityLog.setBounds(0, 21, 441, 61);
 		TxtActivityLog.setText("Hello \nHi");
 		TxtActivityLog.setText("Hello World!\n" + TxtActivityLog.getText());
@@ -212,7 +298,7 @@ public class HomeScreen {
 		});
 		btnHelp.setBounds(467, 446, 81, 25);
 		btnHelp.setText("Help");
-		
+
 		Button btnGraphView = new Button(shlHomeScreen, SWT.NONE);
 		btnGraphView.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -230,7 +316,7 @@ public class HomeScreen {
 		});
 		btnGraphView.setBounds(554, 446, 81, 25);
 		btnGraphView.setText("Graph View");
-		
+
 		Button btnSettings = new Button(shlHomeScreen, SWT.NONE);
 		btnSettings.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -245,11 +331,11 @@ public class HomeScreen {
 					}
 				}
 			}
-			
+
 		});
 		btnSettings.setBounds(641, 446, 81, 25);
 		btnSettings.setText("Settings");
-		
+
 		lblClock = new Label(shlHomeScreen, SWT.NONE);
 		lblClock.setForeground(SWTResourceManager.getColor(SWT.COLOR_DARK_RED));
 		lblClock.setFont(SWTResourceManager.getFont("Calibri", 12, SWT.BOLD));
@@ -258,14 +344,16 @@ public class HomeScreen {
 		lblClock.setText(new SimpleDateFormat("HH:mm:ss").format(new Date()));
 		new Thread() {
 			public void run() {
-				while(true) {
+				while (true) {
 					try {
 						Display display = Display.getDefault();
 						Thread.sleep(1000);
 						display.asyncExec(new Runnable() {
 							@Override
 							public void run() {
-								lblClock.setText(new SimpleDateFormat("d MMM yyyy, HH:mm:ss ").format(new Date()));
+								lblClock.setText(new SimpleDateFormat(
+										"d MMM yyyy, HH:mm:ss ")
+										.format(new Date()));
 							}
 						});
 					} catch (InterruptedException e) {
@@ -274,21 +362,67 @@ public class HomeScreen {
 				}
 			}
 		}.start();
-		
-		
+
 		Label lblLogo = new Label(shlHomeScreen, SWT.NONE);
-		lblLogo.setImage(SWTResourceManager.getImage(HomeScreen.class, "/resource/Uni_Logo.gif"));
+		lblLogo.setImage(SWTResourceManager.getImage(HomeScreen.class,
+				"/resource/Uni_Logo.gif"));
 		lblLogo.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		lblLogo.setBounds(614, 0, 112, 50);
-		
+
 		Group group_1 = new Group(shlHomeScreen, SWT.NONE);
 		group_1.setLocation(10, 0);
 		group_1.setSize(441, 61);
 		group_1.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		group_1.setFont(SWTResourceManager.getFont("Segoe UI", 11, SWT.BOLD));
-		
+
 		Browser browser = new Browser(shlHomeScreen, SWT.NONE);
 		browser.setBounds(467, 439, 64, 32);
-		
+
 	}
+
+	private JFreeChart createChart(final XYDataset dataset) {
+		final JFreeChart result = ChartFactory.createTimeSeriesChart(
+				"Dynamic Line And TimeSeries Chart", "Time", "sugar level",
+				dataset, true, true, false);
+
+		final XYPlot plot = result.getXYPlot();
+
+		plot.setBackgroundPaint(new Color(0xffffe0));
+		plot.setDomainGridlinesVisible(true);
+		plot.setDomainGridlinePaint(Color.lightGray);
+		plot.setRangeGridlinesVisible(true);
+		plot.setRangeGridlinePaint(Color.lightGray);
+
+		ValueAxis xaxis = plot.getDomainAxis();
+		xaxis.setAutoRange(true);
+
+		// Domain axis would show data of 60 seconds for a time
+		xaxis.setFixedAutoRange((6000000.0) * 4.0); // 60 seconds
+		xaxis.setVerticalTickLabels(true);
+
+		ValueAxis yaxis = plot.getRangeAxis();
+		yaxis.setRange(50.0, 440.0);
+
+		return result;
+	}
+
+	/**
+	 * Generates an random entry for a particular call made by time for every
+	 * 1/4th of a second.
+	 *
+	 * @param e
+	 *            the action event.
+	 
+
+	public void actionPerformed(final ActionEvent e) {
+
+		final double factor = 0.9 + 0.2 * Math.random();
+		this.lastValue = this.lastValue * factor;
+
+		final Millisecond now = new Millisecond();
+		this.series.add(new Millisecond(), this.lastValue);
+
+		System.out.println("Current Time in Milliseconds = " + now.toString()
+				+ ", Current Value : " + this.lastValue);
+	}*/
 }
